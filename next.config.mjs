f1211f497b5e50cd888c5e479a5acdf1a90832a5
dks <dkwawan54@gmail.com>
2026-06-01 23:28:23 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {next.NextConfig} */
+const nextConfig = {
+  output: 'export', // Cocok untuk GitHub Pages & PWA Statis
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
